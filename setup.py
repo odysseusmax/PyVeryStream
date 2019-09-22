d50ed23102a897a12d3656f11ae381748c237003
Christy Roys <royschristy@gmail.com>
2019-09-22 13:58:23 +0000

    fix errors while installing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-with copen(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with copen(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
